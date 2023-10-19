a1497445d8e8305e5664b8b069bb843f69e02933
Merve <babutine@yahoo.com>
2023-10-19 15:37:52 +0100

    feat: implement Rest Parameters

--- modern_javascript_exercises/rest_parameters/rest_parameters.js
+++ modern_javascript_exercises/rest_parameters/rest_parameters.js
@@ -1,9 +1,8 @@
 // Instructions can be found in rest_parameters.md
 
 // Add rest parameters here!
-export function add(a, b) {
+export function add(...numbers) {
   // Add a loop here
-  return a + b;
-}
 
-add(1, 2, 3, 4, 5);
+  return numbers.reduce((accumulator, number) => accumulator+number, 0)
+}
